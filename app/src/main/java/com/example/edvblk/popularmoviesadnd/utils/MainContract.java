4d087c0284e2dc994739113381d5c1dda0109e9f
Edvinas Balkaitis <edvblk@telesoftas.com>
2018-02-24 23:33:39 +0200

    Create MainContract interface

---
+++ app/src/main/java/com/example/edvblk/popularmoviesadnd/utils/MainContract.java
@@ -0,0 +1,14 @@
+package com.example.edvblk.popularmoviesadnd.utils;
+
+import com.example.edvblk.popularmoviesadnd.base.BasePresenter;
+
+public interface MainContract {
+    interface View {
+    }
+
+    interface Presenter extends BasePresenter<View> {
+    }
+
+    interface Model {
+    }
+}
