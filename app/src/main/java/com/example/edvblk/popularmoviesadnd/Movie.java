65a8cdab20e72263b3e98585f18c78f603116437
Edvinas Balkaitis <edvblk@telesoftas.com>
2018-02-24 22:36:34 +0200

    Create Movie POJO

---
+++ app/src/main/java/com/example/edvblk/popularmoviesadnd/Movie.java
@@ -0,0 +1,13 @@
+package com.example.edvblk.popularmoviesadnd;
+
+public class Movie {
+    private final String imageUrl;
+
+    public Movie(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
