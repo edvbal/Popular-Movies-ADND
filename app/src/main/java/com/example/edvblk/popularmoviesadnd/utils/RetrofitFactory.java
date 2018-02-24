05bfb294302bd8fe21ef3589afc948738d755aa3
Edvinas Balkaitis <edvblk@telesoftas.com>
2018-02-24 23:21:34 +0200

    Implement BaseFactory in RetrofitFactory

---
+++ app/src/main/java/com/example/edvblk/popularmoviesadnd/utils/RetrofitFactory.java
@@ -0,0 +1,27 @@
+package com.example.edvblk.popularmoviesadnd.utils;
+
+import com.example.edvblk.popularmoviesadnd.BuildConfig;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitFactory implements BaseFactory<Retrofit> {
+    @Override
+    public Retrofit create() {
+        return new Retrofit.Builder()
+                .baseUrl(BuildConfig.MOVIES_SERVICE_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(provideHttpClient())
+                .build();
+    }
+
+    private OkHttpClient provideHttpClient() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor()
+                .setLevel(HttpLoggingInterceptor.Level.BODY);
+        return new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build();
+    }
+}
