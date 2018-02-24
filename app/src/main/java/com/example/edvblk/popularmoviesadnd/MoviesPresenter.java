be5e70760be7b26fafcb68f810a50adcf3c7de82
Edvinas Balkaitis <edvblk@telesoftas.com>
2018-02-25 00:01:36 +0200

    Implement MainContract.Presenter in MoviesPresenter

---
+++ app/src/main/java/com/example/edvblk/popularmoviesadnd/MoviesPresenter.java
@@ -0,0 +1,8 @@
+package com.example.edvblk.popularmoviesadnd;
+
+import com.example.edvblk.popularmoviesadnd.base.BasePresenterImpl;
+
+class MoviesPresenter extends BasePresenterImpl<MainContract.View>
+        implements MainContract.Presenter {
+
+}
