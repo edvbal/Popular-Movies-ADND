b290168ea82ff0d4f5aaa1490c8e3362807d4bc1
Edvinas Balkaitis <edvblk@telesoftas.com>
2018-02-25 12:17:48 +0200

    Check if internet is available with DefaultInternetChecker in MoviesPresenter#onCreate

--- app/src/main/java/com/example/edvblk/popularmoviesadnd/MoviesPresenter.java
+++ app/src/main/java/com/example/edvblk/popularmoviesadnd/MoviesPresenter.java
@@ -1,6 +1,7 @@
 package com.example.edvblk.popularmoviesadnd;
 
 import com.example.edvblk.popularmoviesadnd.base.BasePresenterImpl;
+import com.example.edvblk.popularmoviesadnd.utils.network.DefaultInternetChecker;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
@@ -12,14 +13,19 @@ import static com.example.edvblk.popularmoviesadnd.MainContract.Presenter;
 class MoviesPresenter extends BasePresenterImpl<MainContract.View>
         implements Presenter {
     private final Model model;
+    private DefaultInternetChecker internetChecker;
     private final Disposable disposable = Disposables.disposed();
 
-    MoviesPresenter(Model model) {
+    MoviesPresenter(Model model, DefaultInternetChecker internetChecker) {
         this.model = model;
+        this.internetChecker = internetChecker;
     }
 
     @Override
     public void onCreate() {
+        if (!internetChecker.isInternetAvailable()) {
+            return;
+        }
         model.getMovies()
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe((movies) -> onView(view -> {
--- app/src/main/java/com/example/edvblk/popularmoviesadnd/MoviesPresenterFactory.java
+++ app/src/main/java/com/example/edvblk/popularmoviesadnd/MoviesPresenterFactory.java
@@ -3,6 +3,7 @@ package com.example.edvblk.popularmoviesadnd;
 import android.content.Context;
 
 import com.example.edvblk.popularmoviesadnd.base.BaseApplication;
+import com.example.edvblk.popularmoviesadnd.utils.network.DefaultInternetChecker;
 import com.example.edvblk.popularmoviesadnd.utils.network.MoviesService;
 
 import retrofit2.Retrofit;
@@ -11,6 +12,6 @@ public class MoviesPresenterFactory {
     public MoviesPresenter create(Context context) {
         Retrofit retrofit = BaseApplication.getRetrofit(context);
         MoviesService service = retrofit.create(MoviesService.class);
-        return new MoviesPresenter(new MoviesModel(service));
+        return new MoviesPresenter(new MoviesModel(service), new DefaultInternetChecker(context));
     }
 }
