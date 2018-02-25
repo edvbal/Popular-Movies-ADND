ab5c276b71062cbfe9ccde3ac8f88db93f3d3a4e
Edvinas Balkaitis <edvblk@telesoftas.com>
2018-02-25 11:52:36 +0200

    Create MoviesResultResponse

---
+++ app/src/main/java/com/example/edvblk/popularmoviesadnd/utils/MoviesResultResponse.java
@@ -0,0 +1,9 @@
+package com.example.edvblk.popularmoviesadnd.utils;
+
+public class MoviesResultResponse<T> {
+    private T results;
+
+    public T getResult() {
+        return results;
+    }
+}
