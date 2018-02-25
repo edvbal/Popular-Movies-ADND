28376bd45c632ca09cbd6358142a4c9671c9b530
Edvinas Balkaitis <edvblk@telesoftas.com>
2018-02-25 11:57:04 +0200

    Create MainContract.Presenter#onCreate

--- app/src/main/java/com/example/edvblk/popularmoviesadnd/MoviesPresenter.java
+++ app/src/main/java/com/example/edvblk/popularmoviesadnd/MoviesPresenter.java
@@ -2,7 +2,28 @@ package com.example.edvblk.popularmoviesadnd;
 
 import com.example.edvblk.popularmoviesadnd.base.BasePresenterImpl;
 
-class MoviesPresenter extends BasePresenterImpl<MainContract.View>
-        implements MainContract.Presenter {
+import io.reactivex.disposables.Disposable;
+import io.reactivex.disposables.Disposables;
 
+import static com.example.edvblk.popularmoviesadnd.MainContract.Model;
+import static com.example.edvblk.popularmoviesadnd.MainContract.Presenter;
+
+class MoviesPresenter extends BasePresenterImpl<MainContract.View>
+        implements Presenter {
+    private final Model model;
+    private final Disposable disposable = Disposables.disposed();
+
+    MoviesPresenter(Model model) {
+        this.model = model;
+    }
+
+    @Override
+    public void onCreate() {
+    }
+
+    @Override
+    public void dropView() {
+        disposable.dispose();
+        super.dropView();
+    }
 }
