affb0f017707035d693c811ca2954b249965972b
Edvinas Balkaitis <edvblk@telesoftas.com>
2018-02-25 13:15:38 +0200

    Create constructor in MoviesResultResponse

--- app/src/main/java/com/example/edvblk/popularmoviesadnd/utils/network/MoviesResultResponse.java
+++ app/src/main/java/com/example/edvblk/popularmoviesadnd/utils/network/MoviesResultResponse.java
@@ -3,6 +3,10 @@ package com.example.edvblk.popularmoviesadnd.utils.network;
 public class MoviesResultResponse<T> {
     private T results;
 
+    public MoviesResultResponse(T results) {
+        this.results = results;
+    }
+
     public T getResult() {
         return results;
     }
