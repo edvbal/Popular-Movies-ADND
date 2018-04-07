779ff967c4041d7541b1736240dc508f8b20754a
Edvinas Balkaitis <edvblk@telesoftas.com>
2018-04-07 20:28:30 +0300

    Create empty MovieDetailsActivity

---
+++ app/src/main/java/com/example/edvblk/popularmoviesadnd/details/MovieDetailsActivity.java
@@ -0,0 +1,11 @@
+package com.example.edvblk.popularmoviesadnd.details;
+
+import com.example.edvblk.popularmoviesadnd.R;
+import com.example.edvblk.popularmoviesadnd.base.BaseActivity;
+
+public class MovieDetailsActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_details;
+    }
+}
