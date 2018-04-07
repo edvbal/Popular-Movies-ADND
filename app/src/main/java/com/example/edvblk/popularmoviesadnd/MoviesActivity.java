75a4f42d32acca3cd98c61e01cb3ae102975ddd1
Edvinas Balkaitis <edvblk@telesoftas.com>
2018-04-07 19:43:44 +0300

    Extend BaseActivity in MoviesActivity

--- app/src/main/java/com/example/edvblk/popularmoviesadnd/MoviesActivity.java
+++ app/src/main/java/com/example/edvblk/popularmoviesadnd/MoviesActivity.java
@@ -1,9 +1,9 @@
 package com.example.edvblk.popularmoviesadnd;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 
+import com.example.edvblk.popularmoviesadnd.base.BaseActivity;
 import com.example.edvblk.popularmoviesadnd.utils.ErrorProvider;
 import com.example.edvblk.popularmoviesadnd.utils.ErrorProviderImpl;
 import com.example.edvblk.popularmoviesadnd.utils.image.DefaultImageUrlProvider;
@@ -12,15 +12,12 @@ import com.example.edvblk.popularmoviesadnd.utils.image.GlideImageLoader;
 import java.util.List;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.Unbinder;
 
 import static com.example.edvblk.popularmoviesadnd.MainContract.Presenter;
 
-public class MoviesActivity extends AppCompatActivity implements MainContract.View {
+public class MoviesActivity extends BaseActivity implements MainContract.View {
     @BindView(R.id.recyclerView)
     RecyclerView recyclerView;
-    private Unbinder unbinder;
     private MoviesAdapter adapter;
     private Presenter presenter;
     private ErrorProvider errorProvider;
@@ -28,13 +25,16 @@ public class MoviesActivity extends AppCompatActivity implements MainContract.Vi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         initFields();
         presenter.onCreate();
     }
 
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
     private void initFields() {
-        unbinder = ButterKnife.bind(this);
         errorProvider = new ErrorProviderImpl(recyclerView);
         initPresenter();
         initAdapter();
@@ -71,7 +71,6 @@ public class MoviesActivity extends AppCompatActivity implements MainContract.Vi
     @Override
     protected void onDestroy() {
         presenter.dropView();
-        unbinder.unbind();
         super.onDestroy();
     }
 }
