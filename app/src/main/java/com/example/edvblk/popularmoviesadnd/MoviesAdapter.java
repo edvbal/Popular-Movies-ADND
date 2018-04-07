47d6a80f44ebd6d0778dc023a7785c1aa3170a4c
Edvinas Balkaitis <edvblk@telesoftas.com>
2018-04-07 20:03:01 +0300

    Extend BaseAdapter in MoviesAdapter

--- app/src/main/java/com/example/edvblk/popularmoviesadnd/MoviesAdapter.java
+++ app/src/main/java/com/example/edvblk/popularmoviesadnd/MoviesAdapter.java
@@ -1,18 +1,17 @@
 package com.example.edvblk.popularmoviesadnd;
 
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.edvblk.popularmoviesadnd.base.BaseAdapter;
 import com.example.edvblk.popularmoviesadnd.utils.image.ImageLoader;
 import com.example.edvblk.popularmoviesadnd.utils.image.ImageUrlProvider;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MoviesAdapter extends RecyclerView.Adapter<MoviesViewHolder> {
-    private final List<Movie> items = new ArrayList<>();
+public class MoviesAdapter extends BaseAdapter {
     private final ImageLoader imageLoader;
     private final ImageUrlProvider imageUrlProvider;
 
@@ -26,21 +25,5 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesViewHolder> {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View itemView = inflater.inflate(R.layout.item_movie, parent, false);
         return new MoviesViewHolder(itemView, imageLoader, imageUrlProvider);
-    }
-
-    @Override
-    public void onBindViewHolder(MoviesViewHolder holder, int position) {
-        holder.onBind(items.get(position));
-    }
-
-    @Override
-    public int getItemCount() {
-        return items.size();
-    }
-
-    public void setItems(List<Movie> newItems) {
-        items.clear();
-        items.addAll(newItems);
-        notifyDataSetChanged();
     }
 }
