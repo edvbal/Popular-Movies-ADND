f23e289cb197dbea37187ab86e4496a123e706ad
Edvinas Balkaitis <edvblk@telesoftas.com>
2018-04-07 20:39:44 +0300

    Create MovieDetailsActivity#start

--- app/src/main/java/com/example/edvblk/popularmoviesadnd/details/MovieDetailsActivity.java
+++ app/src/main/java/com/example/edvblk/popularmoviesadnd/details/MovieDetailsActivity.java
@@ -1,9 +1,17 @@
 package com.example.edvblk.popularmoviesadnd.details;
 
+import android.content.Context;
+import android.content.Intent;
+
 import com.example.edvblk.popularmoviesadnd.R;
 import com.example.edvblk.popularmoviesadnd.base.BaseActivity;
 
 public class MovieDetailsActivity extends BaseActivity {
+    public static void start(Context context) {
+        Intent starter = new Intent(context, MovieDetailsActivity.class);
+        context.startActivity(starter);
+    }
+
     @Override
     protected int getLayoutId() {
         return R.layout.activity_details;
